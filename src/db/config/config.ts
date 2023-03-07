fbdce27d2ba5fd9cdf6af9256508baa520e65558
jose-cleiton <cleitons835@gmail.com>
2023-03-06 21:00:26 -0300

    feat: :sparkles: Configurando banco de dados

---
+++ src/db/config/config.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+
+import { Options } from "sequelize";
+
+const config: Options = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT),
+  dialect: 'mysql',
+};
+
+export = config;
